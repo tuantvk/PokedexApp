ab4e9156aa138a01610907066dee554b6a21a0c6
tuantv <tuanhanhphuc123@gmail.com>
2019-10-11 22:28:44 +0700

    add color

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -2,6 +2,14 @@ const Colors = {
   black: '#444',
   gray: '#aab3c7',
   white: '#fff',
+
+  // pokedex color
+  pokedex_green: '#50c1a6',
+  pokedex_red: '#f17569',
+  pokedex_blue: '#59a8f4',
+  pokedex_yellow: '#face4a',
+  pokedex_violet: '#7b518b',
+  pokedex_brow: '#b0736d',
 }
 
 export default Colors;
--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,16 @@ export const scale = (size: number) => width / guidelineBaseWidth * size;
 export const wScale = (size: number) => height / guidelineBaseHeight * size;
 // height
 export const hScale = (size: number, factor: number = 0.5) => size + (scale(size) - size) * factor;
+
+
+// Check backgroundColor pokedex
+const POKEDEX_GREEN = ["Grass", "Poison"];
+const POKEDEX_RED = ["Fire", "Flying"];
+const POKEDEX_BLUE = ["Water", "Ice"];
+const POKEDEX_YELLOW = ["Bug", "Normal", "Electric"];
+const POKEDEX_VIOLET = ["Ground", "Fighting", "Psychic", "Rock"];
+const POKEDEX_BROW = ["Ghost", "Dragon"];
+
+export const checkBgPokedex = (type: string[]) => {
+
+}
