cdea35fe8c666b6645103ff1bc2f84f99e478742
TuanTV <tuanhanhphuc123@gmail.com>
2019-10-12 12:57:21 +0700

    :grey_question:

--- src/containers/Pokedex.tsx
+++ src/containers/Pokedex.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
   StatusBar,
   Dimensions,
+  Image,
 } from 'react-native';
 import {
   Text,
@@ -15,19 +16,29 @@ import Title from '../components/Title';
 import {
   scale,
   checkBgPokedex,
+  wScale,
+  hScale,
 } from '../utils';
 import { Colors } from '../constants';
 import { NavigationStackProp } from 'react-navigation-stack';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 interface PokedexProps {
   navigation: NavigationStackProp<{ pokedex: object }>;
 }
 
+interface PokedexState {
+  navigationState: { index: number, routes: any[] },
+}
+
 const Pokedex = ({
   navigation,
-}: PokedexProps) => {
+}: PokedexProps, PokedexState) => {
+
+  const [index, setIndex] = useState(0);
+  const [navigationState, setNavigationState] = useState();
 
   let pokedex = navigation.getParam('pokedex', '');
 
@@ -51,6 +62,24 @@ const Pokedex = ({
             ))}
           </View>
         </View>
+        <View style={styles.content}>
+          <View style={styles.img}>
+            <Image
+              source={{ uri: pokedex.img }}
+              style={styles.imgPokedex}
+              resizeMode="stretch"
+            />
+          </View>
+          <TabView
+            navigationState={navigationState}
+            renderScene={SceneMap({
+              first: () => <View />,
+              second: () => <View />,
+            })}
+            onIndexChange={index => setIndex(index)}
+            initialLayout={{ width }}
+          />
+        </View>
       </ScrollView>
     </View>
   );
@@ -59,10 +88,12 @@ const Pokedex = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: 'relative',
   },
   header: {
     padding: scale(25),
     height: height / 2,
+    position: 'relative',
   },
   row: {
     flexDirection: 'row',
@@ -74,6 +105,27 @@ const styles = StyleSheet.create({
   },
   num: {
     textAlign: 'right',
+  },
+  img: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: scale(-155),
+    zIndex: 99,
+    left: 0,
+    right: 0,
+  },
+  imgPokedex: {
+    width: wScale(200),
+    height: hScale(200),
+  },
+  content: {
+    backgroundColor: Colors.white,
+    height: height / 1.5,
+    borderTopLeftRadius: scale(25),
+    borderTopRightRadius: scale(25),
+    top: - scale(25),
+    zIndex: 1,
   }
 });
 
